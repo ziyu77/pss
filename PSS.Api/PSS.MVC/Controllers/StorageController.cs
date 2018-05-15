07b2865a065f1c1bc8844fd9f4c44247b9f75578
nzj <1538761548@qq.com>
2018-05-15 16:31:37 +0800

    xxx

---
+++ PSS.Api/PSS.MVC/Controllers/StorageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSS.MVC.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET: Storage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
