577c29ded3b21f113c57c00490d6286817a305e3
hufefan <1983359755@qq.com>
2018-05-18 16:33:24 +0800

    aaa

---
+++ PSS.Api/PSS.MVC/Controllers/DepotsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSS.MVC.Controllers
+{
+    public class DepotsController : Controller
+    {
+        // GET: Depots
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CheckDepot()
+        {
+            //test
+            return View();
+        }
+    }
+}
